8862a71527301efa86c68eec4c139d2a097079cf
aryansaini7078562091-arch <aryansaini7078562091@gmail.com>
2026-04-21 19:21:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,36 +3,18 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.GuildMessages
   ]
 });
 
-// Bot ready
 client.once('ready', () => {
   console.log('Bot online ho gaya!');
 });
 
-// Commands
 client.on('messageCreate', message => {
   if (message.author.bot) return;
 
-  if (message.content === '!hello') {
-    message.reply('Hello bhai 😎');
-  }
-
-  if (message.content === '!phonk') {
-    message.reply('Full phonk vibe 🔥');
-  }
-
-  if (message.content === '!blox') {
-    message.reply('Blox Fruits OP 🍈🔥');
-  }
-
-  if (message.content === '!help') {
-    message.reply('Commands: !hello, !phonk, !blox');
-  }
+  message.reply('Bot working 🔥');
 });
 
-// Login
 client.login(process.env.TOKEN);
